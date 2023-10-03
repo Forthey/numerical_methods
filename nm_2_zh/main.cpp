3f7d232a9aa57ac5535bcf612afa95cb226d70f0
Forthey <0forthey0@gmail.com>
2023-10-03 20:37:24 +0300

    ...

--- nm_2_zh/main.cpp
+++ nm_2_zh/main.cpp
@@ -1,6 +1,46 @@
 ﻿#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+
+class Matrix {
+	double A[10][10];
+	bool initialized = false;
+
+public:
+	void readFromFile(std::string filename) {
+		std::ifstream file;
+		file.open(filename, std::ifstream::in);
+		if (!file.is_open()) {
+			std::cout << "Cannot open the file " << filename << ": not exist" << std::endl;
+			return;
+		}
+		for (int i = 0; i < 10; i++)
+			for (int j = 0; j < 10; j++) {
+				file >> A[i][j];
+			}
+		initialized = true;
+	}
+
+	void display() {
+		if (!initialized) {
+			std::cout << "<Matrix is not initialized>" << std::endl;
+			return;
+		}
+		for (int i = 0; i < 10; i++) {
+			std::cout << "{ ";
+			for (int j = 0; j < 10; j++) {
+				std::cout << std::setw(3) << A[i][j] << " ";
+			}
+			std::cout << "}" << std::endl;
+		}
+	}
+};
 
 int main()
 {
+	Matrix matrix;
+	matrix.readFromFile("matrix.matr");
+	matrix.display();
 	return 0;
 }
--- nm_2_zh/matlab/task2.m
+++ nm_2_zh/matlab/task2.m
@@ -1,1 +1,5 @@
 % ...
+fileHaus = fopen("../matrix/haus_vector.matr", "rt");
+hausVector = fscanf(fileHaus, "%f", [10 1]);
+ort = eye(10) - 2 * hausVector * transpose(hausVector) / (norm(hausVector)^2);
+ort * transpose(ort)
